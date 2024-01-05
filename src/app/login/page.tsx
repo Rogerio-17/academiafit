05f3e3cb2f431fe10907f83cdf90929c1aa52ddc
Rogerio-17 <rogeriojmf10@gmail.com>
2024-01-05 17:16:08 -0300

    Inicando front da tela de login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,23 @@
-import { Teste } from './style';
+import Image from 'next/image';
+import Logo from '../../../public/logo.png';
+import { ImageContainer, InputConateiner, LoginContainer } from './style';
 
 export default function Login() {
   return (
-    <Teste>
-      <h1>Tela de login</h1>
-    </Teste>
+    <LoginContainer>
+      <ImageContainer>
+        <Image src={Logo} alt="Logo"></Image>
+      </ImageContainer>
+      <InputConateiner>
+        <label htmlFor="">Email:</label>
+        <input type="text" />
+      </InputConateiner>
+      <InputConateiner>
+        <label htmlFor="">Senha:</label>
+        <input type="text" />
+      </InputConateiner>
+
+      {/*!!!!!!!!!!!!cria o componente de botão!!!!!!!!!!!!!!!!!!!!!!*/}
+    </LoginContainer>
   );
 }
--- src/app/login/style.ts
+++ src/app/login/style.ts
@@ -2,6 +2,43 @@
 
 import styled from 'styled-components';
 
-export const Teste = styled.div`
-  background: red;
+export const LoginContainer = styled.div`
+  width: 23rem;
+  height: 26rem;
+  margin: auto;
+  margin-top: 4rem;
+  background: ${({ theme }) => theme['gray-300']};
+  border-radius: 8px;
+  overflow: hidden;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const InputConateiner = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 2rem;
+
+  input {
+    width: 20rem;
+    padding: 0.6rem;
+    margin-top: 0.5rem;
+    border-radius: 8px;
+  }
+`;
+
+export const ImageContainer = styled.div`
+  width: 100%;
+  height: 5rem;
+  background: ${({ theme }) => theme.red};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  img {
+    width: 6rem;
+    height: 4rem;
+  }
 `;
